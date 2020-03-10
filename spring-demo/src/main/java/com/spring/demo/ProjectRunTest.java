ec3306f21c8508d223fd96233f1e3cc6090f8e55
wy <wuyun@mamahao.com>
2020-03-10 21:26:47 +0800

    Initial commit

---
+++ spring-demo/src/main/java/com/spring/demo/ProjectRunTest.java
@@ -0,0 +1,17 @@
+package com.spring.demo;
+
+/**
+ * @author wy
+ * @company 妈妈好网络科技
+ * @Classname ProjectRunTest
+ * @Description TODO
+ * @Date 2020/3/10 8:46 下午
+ */
+
+public class ProjectRunTest {
+
+
+	public static void main(String[] args) {
+		System.out.println("hello world 项目正式启动");
+	}
+}
